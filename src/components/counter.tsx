443d4f28bb867e997db08527baf3d205b34f3cbe
Aleksey <alekseykurylev@yandex.ru>
2025-03-09 19:39:45 +0300

    add Counter

---
+++ src/components/counter.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Count update ${count}`);
+    if (count === 5) {
+      setCount(0);
+    }
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}></button>
+    </div>
+  );
+}
